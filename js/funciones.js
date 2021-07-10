ce1d67e34525efe28afca2873b116b823629bcf2
Camilo Sanmiguel <camilo.sanmiguel@gmail.com>
2021-07-09 19:33:47 -0500

    Funcion Igual queda a medias

--- js/funciones.js
+++ js/funciones.js
@@ -50,4 +50,111 @@ btnPorcentaje.onclick = ()=>{ inputText.value += " % "; }
 
 btnIgual.onclick = ()=>{ 
     let text = inputText.value
+    text = text.split(' ').join('')
+    inputText.value = resultado(text);
+}
+
+const resultado = (text)=>{
+    console.log("texto -> ", text);
+    let i = 0;
+    let numero1 = null;
+    let numero2 = null;
+    let primero = true;
+    let accion = null
+    let parentesis = false;
+    do {
+        if(text[i] === "("){
+            let idx = text.indexOf(")");
+            if(idx !== -1){
+                if(numero1===null){
+                    numero1 = resultado(text.substring(i+1,idx));
+                    i = idx;
+                    parentesis = true;
+                }else if(!primero && numero2 === null){
+                    numero2 = resultado(text.substring(i+1,idx));
+                    i = idx;
+                    parentesis = true;
+                }else{
+                    alert("Accion Incorrecta");
+                    inputText.value = "";
+                    break; 
+                }
+            }else{
+                alert("Accion Incorrecta");
+                inputText.value = "";
+                break; 
+            }
+        }else if(text[i] === "1" || text[i] === "2" || text[i] === "3" || text[i] === "4" || text[i] === "5" || text[i] === "6" || text[i] === "7" || text[i] === "8" || text[i] === "9" || text[i] === "0"){
+            if(!parentesis){
+                if(primero){
+                    if(numero1 === null){
+                        numero1 = parseInt(text[i]);
+                    }else{
+                        numero1 = (numero1 * 10) + parseInt(text[i]);
+                    }
+                }else{
+                    if(numero2 === null){
+                        numero2 = parseInt(text[i]);
+                    }else{
+                        numero2 = (numero2 * 10) + parseInt(text[i]);
+                    }
+                }
+            }else{
+                alert("Accion Incorrecta");
+                inputText.value = "";
+                break; 
+            }
+        }else if(text[i] === "%" || text[i] === "/" || text[i] === "x" || text[i] === "-" || text[i] === "+"){
+            if(numero1 === null){
+                alert("Accion Incorrecta");
+                inputText.value = "";
+                break; 
+            }else if(numero2 === null && accion === null){
+                parentesis = false;
+                accion = text[i];
+                primero = false;
+            }else if(numero2 !== null && accion !== null){
+                if(text[i] === "%"){
+                    let temp = numero1;
+                    numero1 = temp % numero2;
+                }else if(text[i] === "/"){
+                    let temp = numero1;
+                    numero1 = temp / numero2;
+                }else if(text[i] === "x"){
+                    let temp = numero1;
+                    numero1 = temp * numero2;
+                }else if(text[i] === "-"){
+                    let temp = numero1;
+                    numero1 = temp - numero2;
+                }else if(text[i] === "+"){
+                    let temp = numero1;
+                    numero1 = temp + numero2;
+                }
+                numero2 = null;
+                accion = null;
+            }
+        }
+        i += 1;
+    } while (i<text.length);
+    
+    if(numero1 !== null && numero2 !== null & accion !== null){
+        if(accion === "%"){
+            return numero1 % numero2;
+        }else if(accion === "/"){
+            return numero1 / numero2;
+        }else if(accion === "x"){
+            return numero1 * numero2;
+        }else if(accion === "-"){
+            return numero1 - numero2;
+        }else if(accion === "+"){
+            return numero1 + numero2;
+        }
+    }else if(numero1 !== null && numero2 === null & accion === null){
+        return numero1;
+    }else{
+        alert("Accion Incorrecta");
+        inputText.value = "";
+        return 0;
+    }
+    
 }
